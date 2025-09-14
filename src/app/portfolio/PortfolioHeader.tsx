8ba75bb7876f63658fda6d623e614adfef06e1c1
CodingAndCodingAgain <158087544+CodingAndCodingAgain@users.noreply.github.com>
2025-09-14 19:27:40 +0200

    refactor: small change in the main title

--- src/app/portfolio/PortfolioHeader.tsx
+++ src/app/portfolio/PortfolioHeader.tsx
@@ -4,7 +4,7 @@ import TextTypeWritter from "../component/TextTypeWritter";
 const PortfolioHeader = ({onComplete}: {onComplete: () => void}) => (
     <Row className="justify-content-center text-center pt-4">
       <Col sm={9} className="d-flex justify-content-center display-4">
-        <TextTypeWritter text="[Project:CV:]" duration={2}/>
+        <TextTypeWritter text="[Project:CV:2025]" duration={2}/>
       </Col>
       <Col sm={9} className="d-flex justify-content-center display-3">
         <TextTypeWritter text="[J_R_L]" duration={2} onComplete={onComplete}/>
